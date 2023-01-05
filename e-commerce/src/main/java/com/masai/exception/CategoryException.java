0344d0b1dde3ec59b511242032aa78e5c85ee541
Puja Deshmukh <105989237+pujadeshmukh1@users.noreply.github.com>
2023-01-05 23:59:48 +0530

    Update CategoryException.java

--- e-commerce/src/main/java/com/masai/exception/CategoryException.java
+++ e-commerce/src/main/java/com/masai/exception/CategoryException.java
@@ -4,12 +4,12 @@ public class CategoryException extends Exception{
 
 	public CategoryException() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public CategoryException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 }
