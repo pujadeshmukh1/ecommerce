cfc271e2f9dd00399b48e0ce27cdbbfb610d39f7
Puja Deshmukh <105989237+pujadeshmukh1@users.noreply.github.com>
2023-01-06 00:06:42 +0530

    Update ShopyApplication.java

--- e-commerce/src/main/java/com/masai/ShopyApplication.java
+++ e-commerce/src/main/java/com/masai/ShopyApplication.java
@@ -10,6 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class ShopyApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(ShopyApplication.class, args);
 	}
 
