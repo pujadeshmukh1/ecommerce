072f04d5a39904a45261ba7a25a25a011a6aff2d
Puja Deshmukh <105989237+pujadeshmukh1@users.noreply.github.com>
2023-01-05 23:42:39 +0530

    Update CartController.java

--- e-commerce/src/main/java/com/masai/controller/CartController.java
+++ e-commerce/src/main/java/com/masai/controller/CartController.java
@@ -23,53 +23,88 @@ import com.masai.service.CartService;
 public class CartController {
 	
 	@Autowired
-	private CartService cs;
+	private CartService cartservice;
 
+	
 	@PostMapping("/add")
 	public ResponseEntity<Cart> addCart(@RequestBody Cart cart) throws CustomerException{
-		Cart c=cs.addCart(cart);
+		
+		Cart c=cartservice.addCart(cart);
+		
 		return new ResponseEntity<Cart>(c,HttpStatus.ACCEPTED);
+		
 	}
 
+	
+	
 	@GetMapping("/view/{id}")
 	public ResponseEntity<Cart> viewCart(@PathVariable("id") int cartId) throws CartException {
-		Cart c=cs.viewCart(cartId);
+		
+		Cart c=cartservice.viewCart(cartId);
+		
 		return new ResponseEntity<Cart>(c,HttpStatus.ACCEPTED);
+		
 	}
 
+	
+	
 	@PutMapping("/addItemIntoCart/{cartId}/{productId}")
 	public ResponseEntity<Cart> addItemIntoCart(@PathVariable("cartId") int cartId, @PathVariable("productId") int productId) throws CartException, ProductException {
-		Cart c=cs.addItemIntoCart(cartId, productId);
+		
+		Cart c=cartservice.addItemIntoCart(cartId, productId);
+		
 		return new ResponseEntity<Cart>(c,HttpStatus.ACCEPTED);
+		
 	}
 
+	
+	
 	@PutMapping("/removeItemFromCart/{cartId}/{productId}")
 	public ResponseEntity<Cart> removeItemFromCart(@PathVariable("cartId") int cartId, @PathVariable("productId") int productId) throws CartException, ProductException {
-		Cart c=cs.removeItemFromCart(cartId, productId);
+		
+		Cart c=  cartservice.removeItemFromCart(cartId, productId);
+		
 		return new ResponseEntity<Cart>(c,HttpStatus.ACCEPTED);
 	}
 
+	
+	
 	@PutMapping("/increaseQuantity/{cartId}/{productId}/{quantity}")
 	public ResponseEntity<Cart> increaseQuantity(@PathVariable("cartId") int cartId, @PathVariable("productId") int productId, @PathVariable("quantity") int quantity) throws CartException, ProductException {
-		Cart c=cs.increaseQuantity(cartId, productId, quantity);
+		
+		Cart c= cartservice.increaseQuantity(cartId, productId, quantity);
+		
 		return new ResponseEntity<Cart>(c,HttpStatus.ACCEPTED);
 	}
 
-	@PutMapping("/decreaseQuantity/{cartId}/{productId}/{quantity}")
-	public ResponseEntity<Cart> decreaseQuantity(@PathVariable("cartId") int cartId, @PathVariable("productId") int productId, @PathVariable("quantity") int quantity) throws CartException, ProductException {
-		Cart c=cs.decreaseQuantity(cartId, productId, quantity);
-		return new ResponseEntity<Cart>(c,HttpStatus.ACCEPTED);
-	}
-
+	
+	
 	@PutMapping("/clearCart/{cartId}")
 	public ResponseEntity<Cart> clearCart(@PathVariable("cartId") int cartId) throws CartException {
-		Cart c=cs.clearCart(cartId);
+		
+		Cart c=cartservice.clearCart(cartId);
+		
+		return new ResponseEntity<Cart>(c,HttpStatus.ACCEPTED);
+	}
+	
+	
+	
+	@PutMapping("/decreaseQuantity/{cartId}/{productId}/{quantity}")
+	public ResponseEntity<Cart> decreaseQuantity(@PathVariable("cartId") int cartId, @PathVariable("productId") int productId, @PathVariable("quantity") int quantity) throws CartException, ProductException {
+		
+		Cart c=cartservice.decreaseQuantity(cartId, productId, quantity);
+		
 		return new ResponseEntity<Cart>(c,HttpStatus.ACCEPTED);
 	}
 
+	
+	
+	
 	@DeleteMapping("/deleteCart/{cartId}")
 	public ResponseEntity<Cart> deleteCart(@PathVariable("cartId") int cartId) throws CartException {
-		Cart c=cs.deleteCart(cartId);
+		
+		Cart c=cartservice.deleteCart(cartId);
+		
 		return new ResponseEntity<Cart>(c,HttpStatus.ACCEPTED);
 	}
 
