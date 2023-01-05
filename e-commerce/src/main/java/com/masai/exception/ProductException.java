42a48c9ba07f1f2a897950be7d0059490dec090e
Puja Deshmukh <105989237+pujadeshmukh1@users.noreply.github.com>
2023-01-06 00:02:12 +0530

    Update ProductException.java

--- e-commerce/src/main/java/com/masai/exception/ProductException.java
+++ e-commerce/src/main/java/com/masai/exception/ProductException.java
@@ -4,12 +4,12 @@ public class ProductException extends Exception{
 
 	public ProductException() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public ProductException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	
