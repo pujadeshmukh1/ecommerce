d05ff665478551d8177f7abb2f1ba62cf3cff40f
Puja Deshmukh <105989237+pujadeshmukh1@users.noreply.github.com>
2023-01-06 00:00:09 +0530

    Update CustomerException.java

--- e-commerce/src/main/java/com/masai/exception/CustomerException.java
+++ e-commerce/src/main/java/com/masai/exception/CustomerException.java
@@ -4,12 +4,12 @@ public class CustomerException extends Exception{
 
 	public CustomerException() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public CustomerException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	
