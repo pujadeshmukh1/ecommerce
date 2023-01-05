a802253a3c2c33b97d1dafe1aba221c7ece36293
Puja Deshmukh <105989237+pujadeshmukh1@users.noreply.github.com>
2023-01-05 23:58:50 +0530

    Update BillException.java

--- e-commerce/src/main/java/com/masai/exception/BillException.java
+++ e-commerce/src/main/java/com/masai/exception/BillException.java
@@ -4,12 +4,12 @@ public class BillException extends Exception{
 
 	public BillException() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public BillException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	
