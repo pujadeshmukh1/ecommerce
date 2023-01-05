b75657afc17095bf38707cdc66e2a52c7264acb5
Puja Deshmukh <105989237+pujadeshmukh1@users.noreply.github.com>
2023-01-05 23:40:16 +0530

    Update BillController.java

--- e-commerce/src/main/java/com/masai/controller/BillController.java
+++ e-commerce/src/main/java/com/masai/controller/BillController.java
@@ -28,34 +28,62 @@ public class BillController {
 	@Autowired
 	private BillService bs;
 	
+	
+	
 	@PostMapping("/add/{id}")
 	public ResponseEntity<Bill> addBill(@RequestBody Bill bill, @PathVariable("id") int orderId) throws BillException, OrderException {
+		
 		Bill b=bs.addBill(bill, orderId);
+		
 		return new ResponseEntity<Bill>(b,HttpStatus.ACCEPTED);
+		
 	}
+	
+	
 	
 	@GetMapping("/view/{id}")
 	public ResponseEntity<Bill> viewBill(@PathVariable("id") int billId) throws BillException {
+		
 		Bill b=bs.viewBill(billId);
+		
 		return new ResponseEntity<Bill>(b,HttpStatus.ACCEPTED);
+		
 	}
+	
+	
 	
 	@GetMapping("/view/{startDate}/{endDate}")
 	public ResponseEntity<List<Bill>> viewBills(@PathVariable("startDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate, @PathVariable("endDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) throws BillException {
+		
 		List<Bill>b=bs.viewBills(startDate, endDate);
+		
 		return new ResponseEntity<List<Bill>>(b,HttpStatus.ACCEPTED);
+		
+	}
+	
+	
+	
+	
+	@DeleteMapping("/delete/{id}")
+	
+	public ResponseEntity<Bill> removeBill(@PathVariable("id") int billId) throws BillException {
+		
+		Bill b=bs.removeBill(billId);
+		
+		return new ResponseEntity<Bill>(b,HttpStatus.ACCEPTED);
 	}
 	
 	@PutMapping("/update")
 	public ResponseEntity<Bill> updateBill(@RequestBody Bill bill) throws BillException {
+		
 		Bill b=bs.updateBill(bill);
+		
 		return new ResponseEntity<Bill>(b,HttpStatus.ACCEPTED);
+		
 	}
 	
-	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<Bill> removeBill(@PathVariable("id") int billId) throws BillException {
-		Bill b=bs.removeBill(billId);
-		return new ResponseEntity<Bill>(b,HttpStatus.ACCEPTED);
-	}
+	
+	
+	
 
 }
