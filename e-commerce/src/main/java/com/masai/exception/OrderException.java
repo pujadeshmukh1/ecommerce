3b330f4867754de18d850d3c0960caf1ccaba036
Puja Deshmukh <105989237+pujadeshmukh1@users.noreply.github.com>
2023-01-06 00:01:52 +0530

    Update OrderException.java

--- e-commerce/src/main/java/com/masai/exception/OrderException.java
+++ e-commerce/src/main/java/com/masai/exception/OrderException.java
@@ -4,12 +4,12 @@ public class OrderException extends Exception{
 
 	public OrderException() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public OrderException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	
