cfe1b18d35123588c6d78dc3634f324434994fce
Puja Deshmukh <105989237+pujadeshmukh1@users.noreply.github.com>
2023-01-05 23:59:27 +0530

    Update CartException.java

--- e-commerce/src/main/java/com/masai/exception/CartException.java
+++ e-commerce/src/main/java/com/masai/exception/CartException.java
@@ -4,12 +4,12 @@ public class CartException extends Exception{
 
 	public CartException() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public CartException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	
